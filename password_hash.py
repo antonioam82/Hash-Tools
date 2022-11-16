dc011a1847f8cfb201b6a5741414849a9d8b0718
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-11-16 17:43:53 +0100

    Update password_hash.py

--- password_hash.py
+++ password_hash.py
@@ -10,7 +10,7 @@ class Program:
     def __init__(self):
 
         self.window = tk.Tk()
-        self.window.geometry("671x460")
+        self.window.geometry("681x460")
         self.window.title("Password  hasher")
         #self.window.resizable(height=tk.FALSE,width=tk.FALSE)
 
@@ -23,9 +23,9 @@ class Program:
         self.password_Frame.grid(row=0,column=0,padx=19,pady=30)
         tk.Label(self.password_Frame,text="Enter Password:").grid(pady=5,row=0,column=0)
         tk.Label(self.password_Frame,text="Confirm password:"+" "*6).grid(pady=5,row=1,column=0)
-        self.Entry1 = tk.Entry(self.password_Frame,width=75)
+        self.Entry1 = tk.Entry(self.password_Frame,width=77)
         self.Entry1.grid(pady=1,row=0,column=1)
-        self.Entry2 = tk.Entry(self.password_Frame,width=75)
+        self.Entry2 = tk.Entry(self.password_Frame,width=77)
         self.Entry2.grid(pady=1,row=1,column=1)
         
 
@@ -33,10 +33,10 @@ class Program:
         self.output_Frame.grid(row=1,column=0,padx=19,pady=0)
         tk.Label(self.output_Frame,text="MD5").grid(pady=5,row=0,column=0)
         tk.Label(self.output_Frame,text="SHA1").grid(pady=5,row=1,column=0)
-        tk.Label(self.output_Frame,text="SHA224").grid(pady=5,row=2,column=0)
-        tk.Label(self.output_Frame,text="SHA256").grid(pady=5,row=3,column=0)
-        tk.Label(self.output_Frame,text="SHA384").grid(pady=5,row=4,column=0)
-        tk.Label(self.output_Frame,text="SHA512").grid(pady=5,row=5,column=0)
+        tk.Label(self.output_Frame,text="SHA224    ").grid(pady=5,row=2,column=0)
+        tk.Label(self.output_Frame,text="SHA256    ").grid(pady=5,row=3,column=0)
+        tk.Label(self.output_Frame,text="SHA384    ").grid(pady=5,row=4,column=0)
+        tk.Label(self.output_Frame,text="SHA512    ").grid(pady=5,row=5,column=0)
         self.md5Out = tk.Entry(self.output_Frame,width=75)
         self.md5Out.grid(pady=1,row=0,column=1)
         self.sha1Out = tk.Entry(self.output_Frame,width=75)
